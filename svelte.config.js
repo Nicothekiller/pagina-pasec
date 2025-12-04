da4ec0a043402fea6e52ac882ed999c3afc20d68
Nicothekiller <nicolasnaran@gmail.com>
2025-12-04 18:53:12 -0500

    fail at fixing website

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { appDir: 'app', adapter: adapter({fallback: "src/404.html"}) }
 };
 
 export default config;
